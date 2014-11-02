c62ba2e9abcb0fa25c96c141d7ff5ce57392740c
wolterzo <wolterzo@grinnell.edu>
2014-11-02 16:37:21 -0600

    Write MergeAnalyst to comapre run times of merge methods.

---
+++ src/taojava/labs/sorting/MergeAnalyst.java
@@ -0,0 +1,36 @@
+package taojava.labs.sorting;
+
+import java.io.PrintWriter;
+
+public class MergeAnalyst
+{
+
+  public static void main(String[] args)
+  {
+    PrintWriter pen = new PrintWriter(System.out, true);
+    @SuppressWarnings("unchecked")
+    Sorter<Integer>[] sorters =
+        (Sorter<Integer>[]) new Sorter[] { new MergeSorter<Integer>(),
+                                          new MergeSorterB<Integer>(),
+                                          new IterativeMergeSorter<Integer>() };
+    String[] sorterNames = { "MergeSorter", "MergeSorterB", "IterativeMerge" };
+
+    @SuppressWarnings("unchecked")
+    ArrayBuilder<Integer>[] builders =
+        (ArrayBuilder<Integer>[]) new ArrayBuilder[] {
+                                                      SorterAnalyzer.randomIntArrBuilder,
+                                                      SorterAnalyzer.increasingIntArrBuilder,
+                                                      SorterAnalyzer.reverseIntArrBuilder,
+                                                      SorterAnalyzer.mostlyOrderedIntArrBuilder };
+    String[] builderNames =
+        { "Random", "Increasing", "Reverse", "Mostly Ordered" };
+
+    SorterAnalyzer.combinedAnalysis(pen, sorters, sorterNames,
+                                    SorterAnalyzer.standardIntComparator,
+                                    builders, builderNames);
+
+  } // main(String[])
+  // The first two implementations of merge sort (recursive) take virtually the
+  // same amount of time. Iterative merge sort take slightly less time than the
+  // other methods. Sorting random arrays takes longer than any other type. 
+} // MergeAnalyst
