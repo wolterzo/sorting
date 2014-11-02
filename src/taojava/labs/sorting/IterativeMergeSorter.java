e1c11b03fe08181149da28b97012e3b574ddf767
wolterzo <wolterzo@grinnell.edu>
2014-11-02 16:36:37 -0600

    Implement iterative merge sort

--- src/taojava/labs/sorting/IterativeMergeSorter.java
+++ src/taojava/labs/sorting/IterativeMergeSorter.java
@@ -21,13 +21,30 @@ public class IterativeMergeSorter<T>
   {
     // STUB
     int size = 1;
-    while (size < vals.length)
+    int length = vals.length;
+    @SuppressWarnings("unchecked")
+    T[] scratch = (T[]) new Object[length];
+    while (size < length)
       {
+        scratch = vals.clone();
         // Merge neighboring subarrays of size size
-        // FILL IN!
+        int i = 0;
+        while (i + 2 * size <= length)
+          {
+            Utils.merge(order, scratch, i, i + size, scratch, i + size, i + 2
+                                                                        * size,
+                        vals, i, i + 2 * size);
+            i += 2 * size;
+          } // while
+        if (2 * size > length)
+          {
+            Utils.merge(order, scratch, 0, size, scratch, size, length, vals,
+                        0, length);
+          }//if
         // The merged subarrays are now twice as large
         size *= 2;
       } // while
+
     return vals;
   } // sorti(T[], Comparator<T>)
 } // IterativeMergeSorter<T>
