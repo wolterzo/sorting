5dcb5bee3232d4fce9f6bdd51bcc9219f440f574
wolterzo <wolterzo@grinnell.edu>
2014-11-04 17:54:50 -0600

    Ran and analyzed quicksort algorithms.

--- src/taojava/labs/sorting/QuicksortAnalyzer.java
+++ src/taojava/labs/sorting/QuicksortAnalyzer.java
@@ -13,7 +13,7 @@ public class QuicksortAnalyzer
                                           new NewerQuicksorter<Integer>(),
                                           new NewRandomQuicksorter<Integer>(),
                                           new NewMedianQuicksorter<Integer>() };
-    String[] sorterNames = { "New", "Newer", "Random", "Median" };
+    String[] sorterNames = { "First", "Middle", "Random", "Median" };
 
     @SuppressWarnings("unchecked")
     ArrayBuilder<Integer>[] builders =
@@ -30,3 +30,18 @@ public class QuicksortAnalyzer
                                     builders, builderNames);
   } // main(String[])
 } // QuickSortAnalyzer
+
+// For arrays that are in increasing, mostly ordered, or reversed order, 
+// picking the middle element as the pivot gives the best run times. For 
+// randomly generated arrays, how we pick the pivot doesn't seem to make 
+// a big difference in run time. 
+
+// When picking the first element as the pivot, it works fine for random
+// arrays. For increasing arrays, it is very inefficient, as the partition 
+// performs a lot of swaps which then makes the array less ordered.
+// Reversed arrays are even more inefficient. The results are better for 
+// mostly ordered arrays, but still about as twice as slow as other pivots.
+
+// We find it interesting that picking the median of three random elements
+// is slightly less efficient than picking one random element. Maybe 
+// because finding the median takes more time? We're not sure.
